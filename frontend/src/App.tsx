e486a195cb68133f38824ef3a89c7977513215eb
benjaminmerchin <benjaminmerchin@gmail.com>
2022-04-25 17:32:34 +0200

    header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header';
+import Chat from './pages/Chat';
+import Game from './pages/Game';
+import Home from './pages/Home';
+import LoginRegister from './pages/LoginRegister';
+
 function App() {
   return (
-    <div>ECO+</div>
+    <>
+      <Router>
+        <div>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/loginregister" element={<LoginRegister />} />
+            <Route path="/chat" element={<Chat />} />
+            <Route path="/game" element={<Game />} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
