faccac93441a9f7484a205cfe1e875c6bb327ac2
ttranche <ttranche@student.42.fr>
2022-06-30 00:21:10 +0200

    Create profile page

---
+++ frontend/src/pages/CreateProfile.tsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { RootStateOrAny, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import logo from './assets/logo.svg';
+
+function CreateProfile() {
+  const { user } = useSelector((state: RootStateOrAny) => state.auth);
+  const navigate = useNavigate();
+
+  const goProfile = () => {
+    localStorage.setItem("didCreate", "false");
+    navigate('/userprofile/' + user.id);
+  }
+
+  const validate = () => {
+    localStorage.setItem("didCreate", "false");
+    navigate('/');
+  }
+
+  return (
+    <div className="landingPage">
+      <img src={logo} style={{width: "300px"}}></img>
+
+      <div style={{padding: "40px"}}>
+        <p>Please complete your profile on your profile page or continue</p>
+      </div>
+
+      <button className="createButton" title="Complete Profile" onClick={goProfile}>Profile page</button>
+      <br/>
+      <button className="createButton" title="Continue" onClick={validate}>Continue</button>
+
+    </div>
+  );
+}
+
+export default CreateProfile;
