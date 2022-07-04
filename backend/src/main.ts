156e8d1bc4f6e6999a9b5e78b91cbbbe39ee2d12
ttranche <ttranche@student.42.fr>
2022-07-04 23:01:53 +0200

    Fixed image upload

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import express, { urlencoded, json } from 'express';
 import * as cookieParser from 'cookie-parser';
 
 const BACKEND_PORT = process.env.BACKEND_PORT || 5050;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-//   app.enableCors({ origin: FRONT_DOMAIN, credentials: true });
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
+
   //rajoute le prefixe api devant toutes les routes
   app.setGlobalPrefix('/api');
   app.use(cookieParser());
