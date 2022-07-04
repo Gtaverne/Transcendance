0c50911a94de40bce129b819cce725016bd2ec47
benjaminmerchin <benjaminmerchin@gmail.com>
2022-07-04 19:03:56 +0200

    clean

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,22 +1,13 @@
 import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoomsController } from './rooms/rooms.controller';
-import { MessagesController } from './messages/messages.controller';
-import { GamesController } from './games/games.controller';
 import { UsersModule } from './users/users.module';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-import { UsersController } from './users/users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { config } from './orm.config';
-import { RoomsService } from './rooms/rooms.service';
-import { MessagesService } from './messages/messages.service';
-import { GamesService } from './games/games.service';
 import { GamesModule } from './games/games.module';
 import { RoomsModule } from './rooms/rooms.module';
 import { MessagesModule } from './messages/messages.module';
-import { TutoModule } from './tuto/tuto.module';
-import { MicrocdnController } from './microcdn/microcdn.controller';
 import { MicrocdnModule } from './microcdn/microcdn.module';
 import { AchievementsModule } from './achievements/achievements.module';
 
@@ -27,7 +18,6 @@ import { AchievementsModule } from './achievements/achievements.module';
     RoomsModule,
     MessagesModule,
     TypeOrmModule.forRoot(config),
-    TutoModule,
     MicrocdnModule,
     AchievementsModule,
   ],
